1184c794a9771974ede04ecee3e0e56f5ea4f6f4
Ivan Pavluk <chayleaf@protonmail.com>
2021-09-23 02:39:38 +0700

    v0.1.0

---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/env sh
+curl https://www.iana.org/assignments/urn-namespaces/urn-namespaces-1.csv -o urn-namespaces-1.csv -s
+curl https://www.iana.org/assignments/urn-namespaces/urn-namespaces-2.csv -o urn-namespaces-2.csv -s
